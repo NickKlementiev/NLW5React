56f08fadc0608f98160e0c1e8fd57f24a1d52c31
NickKlementiev <nklementiev@hotmail.com>
2021-04-20 11:43:45 -0300

    :sparkles: Second lesson

---
+++ src/pages/index.tsx
@@ -0,0 +1,54 @@
+// SPA (Single Page Applicationa)
+
+/*
+useEffect(() => {
+    fetch('http://localhost:3333/episodes')
+        .then(response => response.json())
+        .then(data => console.log(data));
+}, []);*/
+
+// SSR (Server Side Rendering)
+
+/*
+ export default function Home(props) {
+     return (
+         <div>
+             <h1>Index</h1>
+             <p>{JSON.stringify(props.episodes)}</p>
+         </div>
+     );
+ }
+
+ export async function getServerSideProps() {
+     const response = await fetch('http://localhost:3333/episodes');
+     const data = await response.json();
+
+     return {
+         props: {
+             episodes: data
+         }
+     }
+ }*/
+
+// SSG (Static Site Generation)
+
+export default function Home(props) {
+    return (
+        <div>
+            <h1>Index</h1>
+            <p>{JSON.stringify(props.episodes)}</p>
+        </div>
+    );
+}
+
+export async function getStaticProps() {
+    const response = await fetch('http://localhost:3333/episodes');
+    const data = await response.json();
+
+    return {
+        props: {
+            episodes: data
+        },
+        revalidate: 60 * 60 * 8
+    }
+}
